43172ea8500dcc299174b610475279704871468c
Kevin Orta <kevino6532@hstat.org>
2024-10-25 15:52:25 -0400

    add router and routes

---
+++ client/src/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from './App';
+import { LoginPage } from "./pages/LoginPage";
+import { SurveyPage } from "./pages/SurveyPage";
+// import {  } from "./pages/";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+    },
+    {
+        path: '/login',
+        element: <LoginPage/>,
+    },
+    {
+        path: '/survey',
+        element: <SurveyPage/>,
+    },
+])
