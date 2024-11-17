0768290944dedb920253e28f300e67cd233db8a7
JNikolo <125705821+JNikolo@users.noreply.github.com>
2024-11-17 12:16:25 -0500

    fix: JS standard correction

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,18 @@
-import Express from "express";
-import registerRouter from "./routes/registerRoutes.js";
-import cors from "cors";
+import Express from 'express'
+import registerRouter from './routes/registerRoutes.js'
+import cors from 'cors'
 
-const app = Express();
+const app = Express()
 
-app.use(Express.json());
-app.use(cors());
-app.use("/register", registerRouter);
+app.use(Express.json())
+app.use(cors())
+app.use('/register', registerRouter)
 
-app.get("/", (req, res) => {
-  console.log("got requested");
-  res.send("Hello World");
-});
+app.get('/', (req, res) => {
+  console.log('got requested')
+  res.send('Hello World')
+})
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  console.log('Server is running on port 3000')
+})
--- server/src/middlewares/clerk.js
+++ server/src/middlewares/clerk.js
@@ -1,7 +1,7 @@
-import { createClerkClient } from "@clerk/express";
-import dotenv from "dotenv";
-dotenv.config();
+import { createClerkClient } from '@clerk/express'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const clerkClient = createClerkClient({
-  secretKey: process.env.CLERK_SECRET_KEY,
-});
+  secretKey: process.env.CLERK_SECRET_KEY
+})
