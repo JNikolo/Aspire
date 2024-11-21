2bf1e53dcaaff9e5d6d2e12473c8059cbd9509f0
JNikolo <125705821+JNikolo@users.noreply.github.com>
2024-11-21 01:50:58 -0500

    fix: Add the fallback route for SSO registration

--- server/src/routes/registerRoutes.js
+++ server/src/routes/registerRoutes.js
@@ -1,36 +1,69 @@
-import { Router } from 'express'
-import { prisma } from '../middlewares/prisma.js'
-import { clerkClient } from '../middlewares/clerk.js'
-import { clerkMiddleware } from '@clerk/express'
-const registerRouter = Router()
+import { Router } from "express";
+import { prisma } from "../middlewares/prisma.js";
+import { clerkClient } from "../middlewares/clerk.js";
+import { clerkMiddleware } from "@clerk/express";
+const registerRouter = Router();
 
-registerRouter.use(clerkMiddleware())
+registerRouter.use(clerkMiddleware());
 
-registerRouter.post('/create-after-signup', async (req, res) => {
+registerRouter.post("/create-after-signup", async (req, res) => {
   try {
-    const { userId } = req.auth
+    const { userId } = req.auth;
 
-    const user = await clerkClient.users.getUser(userId)
+    const user = await clerkClient.users.getUser(userId);
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' })
+      return res.status(404).json({ error: "User not found" });
     }
 
     await prisma.user.create({
       data: {
         authId: user.id,
         email: user.emailAddresses[0].emailAddress,
-        username: user.username
-      }
-    })
+        username: user.username,
+      },
+    });
 
-    res.json({ message: 'User created successfully' })
+    res.json({ message: "User created successfully" });
   } catch (err) {
-    console.error('Error during create-after-signup:', err)
+    console.error("Error during create-after-signup:", err);
     res
       .status(500)
-      .json({ error: 'An error occurred during create-after-signup' })
+      .json({ error: "An error occurred during create-after-signup" });
   }
-})
+});
 
-export default registerRouter
+registerRouter.post("/sso-callback", async (req, res) => {
+  try {
+    const { userId } = req.auth;
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { authId: user.id },
+    });
+
+    if (!existingUser) {
+      await prisma.user.create({
+        data: {
+          authId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+          username: user.username,
+          profilePicture: user.imageUrl,
+        },
+      });
+
+      res.json({ message: "User logged in successfully", newUser: true });
+    }
+
+    res.json({ message: "User logged in successfully", newUser: false });
+  } catch (err) {
+    console.error("Error during sso-callback:", err);
+    res.status(500).json({ error: "An error occurred during sso-callback" });
+  }
+});
+export default registerRouter;
