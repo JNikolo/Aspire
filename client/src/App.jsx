1f21de173b41fa5fec0200c546f7d9aa50a5ca9a
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2024-12-10 02:19:45 -0500

    added navBar to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { DashboardPage } from "./pages/DashboardPage";
 import { NavBar } from "./components/NavBar";
-import { LoginPage } from "./pages/LoginPage";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <NavBar />;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
