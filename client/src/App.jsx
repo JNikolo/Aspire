28517dee5ac2ebcd4663eded3366214c4bd1ba1e
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2024-12-09 23:28:26 -0500

    added navBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { DashboardPage } from "./pages/DashboardPage";
+import { NavBar } from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
-    <DashboardPage />
+    <NavBar />
   );
 }
 
